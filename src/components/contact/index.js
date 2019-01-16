95a74c83d32283fc678c7770d5d120080e6293fa
Tyler <tylerthomasallen@gmail.com>
2019-01-15 17:28:54 -0800

    Add full page background img

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './styles.scss';
 
 class Contact extends Component {
   render() {
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './styles.scss';
 import tyler from './tyler.jpg';
 import Contact from '../contact';
 
